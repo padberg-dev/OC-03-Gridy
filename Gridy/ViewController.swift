65f69a6543202bbc71d2de3949ed68acc20e1075
Ra-Fa-L <padbi.raf@gmail.com>
2018-12-12 17:49:25 +0100

    Initial Commit

---
+++ Gridy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gridy
+//
+//  Created by Rafal Padberg on 12.12.18.
+//  Copyright © 2018 Rafal Padberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
